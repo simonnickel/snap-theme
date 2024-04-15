e2e0f1eb840ff3f1dd7cbc4923dedb633e411693
Simon Nickel <simonnickel@googlemail.com>
2024-04-15 12:03:11 +0200

    Update package

--- Package.swift
+++ Package.swift
@@ -5,19 +5,35 @@ import PackageDescription
 
 let package = Package(
     name: "snap-theme",
+	platforms: [
+		.iOS(.v17), .macOS(.v14)
+	],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SnapTheme",
-            targets: ["SnapTheme"]),
+            targets: ["SnapTheme"]
+		),
     ],
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+		.package(url: "https://github.com/simonnickel/snap-core.git", branch: "main"),
+	],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SnapTheme"),
+            name: "SnapTheme",
+			dependencies: [
+				.product(name: "SnapCore", package: "snap-core")
+			],
+			swiftSettings: [
+				.enableExperimentalFeature("StrictConcurrency")
+			]
+		),
         .testTarget(
             name: "SnapThemeTests",
-            dependencies: ["SnapTheme"]),
+            dependencies: ["SnapTheme"]
+		),
     ]
 )
