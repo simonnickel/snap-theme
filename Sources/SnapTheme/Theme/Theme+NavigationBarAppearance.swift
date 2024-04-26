46d8c95a884935e7f692070c1473ecba2a972441
Simon Nickel <simonnickel@googlemail.com>
2024-04-26 15:12:33 +0200

    [wip] Navigation title font design

--- Sources/SnapTheme/Theme/Theme+NavigationBarAppearance.swift
+++ Sources/SnapTheme/Theme/Theme+NavigationBarAppearance.swift
@@ -24,11 +24,16 @@ extension Theme {
 					let standardAppearance = UINavigationBarAppearance()
 					standardAppearance.configureWithDefaultBackground()
 					
-					// TODO: Set font from theme. Needs to get UIFont from Font.
 					let color = theme.color(for: .navigationTitle, in: environment)
+					
+					// TODO idea: Set font from theme. Needs to get UIFont from Font.
+					// Converting Font to UIFont is a bit tricky. This will at least use fontDesignDefault of the Theme.
+					// Only applied on app start.
+					let font = UIFont.preferredFont(for: .largeTitle, weight: .bold, design: theme.fontDesignDefault.valueUIFontDescriptor)
 					
 					let attributes: [NSAttributedString.Key : Any] = [
 						.foregroundColor: UIColor(color),
+						.font: font,
 					]
 					standardAppearance.titleTextAttributes = attributes
 					standardAppearance.largeTitleTextAttributes = attributes
@@ -39,4 +44,29 @@ extension Theme {
 		}
 	}
 	
+}
+
+
+// TODO: This does not belong here!
+extension UIFont {
+	static func preferredFont(for style: UIFont.TextStyle, weight: UIFont.Weight, design: UIFontDescriptor.SystemDesign = .rounded) -> UIFont {
+		let defaultFont = UIFont.preferredFont(forTextStyle: style)
+		var fontDescriptor = defaultFont.fontDescriptor
+		
+		// Customizing weight
+		fontDescriptor = fontDescriptor.addingAttributes([
+			UIFontDescriptor.AttributeName.traits: [
+				UIFontDescriptor.TraitKey.weight: weight.rawValue
+			]
+		])
+		
+		// Customizing design
+		if let descriptor = fontDescriptor.withDesign(design) {
+			fontDescriptor = descriptor
+		}
+		
+		// Setup the font to be auto-scalable
+		let metrics = UIFontMetrics(forTextStyle: style)
+		return metrics.scaledFont(for: UIFont(descriptor: fontDescriptor, size: defaultFont.pointSize))
+	}
 }
