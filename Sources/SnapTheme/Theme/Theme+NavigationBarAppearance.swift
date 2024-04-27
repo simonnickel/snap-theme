5c21011b9bcd8115892a8b6ebe0eb0eed029d1ac
Simon Nickel <simonnickel@googlemail.com>
2024-04-27 15:50:14 +0200

    Move preferred font to SnapCore

--- Sources/SnapTheme/Theme/Theme+NavigationBarAppearance.swift
+++ Sources/SnapTheme/Theme/Theme+NavigationBarAppearance.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import UIKit
+import SnapCore
 
 // TODO FB: SwiftUI should support styling of navigation bar.
 
@@ -19,7 +20,7 @@ extension Theme {
 		
 		public func body(content: Content) -> some View {
 			return content
-#if !os(macOS)
+#if !os(macOS) // TODO macOS: Find a way to customize the macOS navigation title.
 				.onAppear() {
 					let standardAppearance = UINavigationBarAppearance()
 					standardAppearance.configureWithDefaultBackground()
@@ -29,7 +30,7 @@ extension Theme {
 					// TODO idea: Set font from theme. Needs to get UIFont from Font.
 					// Converting Font to UIFont is a bit tricky. This will at least use fontDesignDefault of the Theme.
 					// Only applied on app start.
-					let font = UIFont.preferredFont(for: .largeTitle, weight: .bold, design: theme.fontDesignDefault.valueUIFontDescriptor)
+					let font = UIFont.preferredFont(for: Theme.FontDefinition.TextStyle.largeTitle.valueUIKit, weight: .bold, design: theme.fontDesignDefault.valueUIFontDescriptor)
 					
 					let attributes: [NSAttributedString.Key : Any] = [
 						.foregroundColor: UIColor(color),
@@ -44,29 +45,4 @@ extension Theme {
 		}
 	}
 	
-}
-
-
-// TODO: This does not belong here!
-extension UIFont {
-	static func preferredFont(for style: UIFont.TextStyle, weight: UIFont.Weight, design: UIFontDescriptor.SystemDesign = .rounded) -> UIFont {
-		let defaultFont = UIFont.preferredFont(forTextStyle: style)
-		var fontDescriptor = defaultFont.fontDescriptor
-		
-		// Customizing weight
-		fontDescriptor = fontDescriptor.addingAttributes([
-			UIFontDescriptor.AttributeName.traits: [
-				UIFontDescriptor.TraitKey.weight: weight.rawValue
-			]
-		])
-		
-		// Customizing design
-		if let descriptor = fontDescriptor.withDesign(design) {
-			fontDescriptor = descriptor
-		}
-		
-		// Setup the font to be auto-scalable
-		let metrics = UIFontMetrics(forTextStyle: style)
-		return metrics.scaledFont(for: UIFont(descriptor: fontDescriptor, size: defaultFont.pointSize))
-	}
 }
